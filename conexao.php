2dc70c1378a218b7d147c46f0b712cef12ffd887
Alex Sandro de Almeida <sandergost@gmail.com>
2018-10-22 17:38:07 -0300

    Aula 2 - 24:17
    Parei na function para acertar data

--- conexao.php
+++ conexao.php
@@ -1,14 +1,37 @@
 <?php 
-if ($_POST["acao"]=="inserir"){
-    inserirPessoa();    
+if(isset($_POST["acao"])){
+    if ($_POST["acao"]=="inserir"){
+        inserirPessoa();    
+    }
+}
+
+function abrirBanco(){
+    $conexao = new mysqli("localhost", "root", "", "agenda");
+    return $conexao;
 }
 
 function inserirPessoa(){
+    $banco = abrirBanco();
     $banco = new mysqli("localhost", "root", "", "agenda");
     $sql = "INSERT INTO pessoa(nome, nascimento, endereco, telefone) VALUES ('{$_POST["nome"]}','{$_POST["nascimento"]}','{$_POST["endereco"]}','{$_POST["telefone"]}')";
     $banco->query($sql);
     $banco->close();
+    voltarIndex();
 }
 
-header("Location:index.php");
+function selectAllPessoa(){
+    $banco = abrirBanco();
+    $sql = "SELECT * FROM pessoa ORDER BY nome";
+    $resultado = $banco->query($sql);
+    while ($row = mysqli_fetch_array($resultado)){
+        $grupo[] = $row;
+    }
+    
+    return $grupo;
+}
+
+function voltarIndex(){
+
+    header("Location:index.php");
+}    
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<!DOCTYPE html>
+<?php
+    include("conexao.php");
+    $grupo = selectAllPessoa();    
+?>
 <html>
 <head>
     <meta charset="utf-8">
@@ -7,8 +12,45 @@
     <h1>Agenda pessoal</h1>
     <a href="https://www.youtube.com/watch?v=t9Yoq3SfFsU&index=1&list=PLfs7O23seKWnn5YT7GarQHIAAJDgB4ZZ3" target="_blank">Video Aula</a> <br> <br>
     <a href="inserir.php">Add Pessoa</a>
+    
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Nascimento</th>
+                <th>Telefone</th>
+                <th>Endereço</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <?php 
+                foreach ($grupo as $pessoa) { ?> 
+                
+                <tr>
+                    <td><?=$pessoa["nome"]?></td>
+                    <td><?=$pessoa["nascimento"]?></td>
+                    <td><?=$pessoa["telefone"]?></td>
+                    <td><?=$pessoa["endereco"]?></td>
+                    <td>Editar</td>
+                    <td>Excluir</td>
+                </tr> 
+                   
+            <?php } ?>    
+        </tbody>
+        
+    </table>
+    
     <?php 
-        //put your code here
+        function formatoData($data){
+            $array = explode("-", $data);
+            // $data = 2016-04-14
+            // $array[0]= 2016, $array[1] = 04 e $array[2]
+        }
+    
     ?>
+    
 </body>
 </html>
